f2129527b3911bc2b279a223dedb24417a225a46
Anna Borzenkowa <crownedjocker@gmail.com>
2023-07-04 12:43:11 +0300

    Initial commit

---
+++ src/test/java/ru/netology/javaqa/ShopRepositoryTest.java
@@ -0,0 +1,49 @@
+package ru.netology.javaqa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShopRepositoryTest {
+
+    @Test
+    void shouldAddItem() {
+        ShopRepository repo = new ShopRepository();
+        Product product1 = new Product(28, "хлеб", 15);
+        Product product2 = new Product(45, "молоко", 66);
+        Product product3 = new Product(86, "сыр", 97);
+
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+
+        Product[] expected = {product1, product2, product3};
+        Product[] actual = repo.findAll();
+        Assertions.assertArrayEquals(expected,actual);
+    }
+
+    @Test
+    void shouldFindAll() {
+    }
+
+    @Test
+    void shouldRemoveItem() {
+        ShopRepository repo = new ShopRepository();
+        Product product1 = new Product(28, "хлеб", 15);
+        Product product2 = new Product(45, "молоко", 66);
+        Product product3 = new Product(86, "сыр", 97);
+
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+
+        repo.remove(45);
+
+        Product[] expected = {product1, product3};
+        Product[] actual = repo.findAll();
+
+        Assertions.assertArrayEquals(expected,actual);
+    }
+
+}
